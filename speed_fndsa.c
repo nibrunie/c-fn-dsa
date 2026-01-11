8d20a8c0b99f025cefbdd2a12f0e6cc360e74315
Nicolas Brunie <nibrunie@gmail.com>
2026-01-11 04:39:08 -0800

    Adding PERF based cycle retrieval

--- speed_fndsa.c
+++ speed_fndsa.c
@@ -84,7 +84,51 @@
 
 #include "fndsa.h"
 
-#if defined __x86_64__ || defined _M_X64 || defined __i386__ || defined _M_IX86
+#if defined PERF_CYCLES
+
+#include <asm/unistd.h>
+#include <linux/perf_event.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                            int cpu, int group_fd, unsigned long flags) {
+  return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+}
+
+static int perf_fd = -1;
+
+static void __attribute__((constructor)) perf_init(void) {
+  struct perf_event_attr pe;
+
+  memset(&pe, 0, sizeof pe);
+  pe.type = PERF_TYPE_HARDWARE;
+  pe.size = sizeof pe;
+  pe.config = PERF_COUNT_HW_CPU_CYCLES;
+  pe.disabled = 1;
+  pe.exclude_kernel = 1;
+  pe.exclude_hv = 1;
+
+  perf_fd = perf_event_open(&pe, 0, -1, -1, 0);
+  if (perf_fd == -1) {
+    fprintf(stderr, "Error opening leader %llx\n", pe.config);
+    exit(EXIT_FAILURE);
+  }
+  ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+static inline uint64_t core_cycles(void) {
+  uint64_t x;
+
+  if (read(perf_fd, &x, sizeof x) == -1) {
+    return 0;
+  }
+  return x;
+}
+
+#elif defined __x86_64__ || defined _M_X64 || defined __i386__ ||              \
+    defined _M_IX86
 #include <immintrin.h>
 #ifdef _MSC_VER
 /* On Windows, the intrinsic is called __readpmc(), not __rdpmc(). But it
